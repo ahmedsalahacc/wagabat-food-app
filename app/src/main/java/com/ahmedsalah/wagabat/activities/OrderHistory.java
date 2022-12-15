5fcbb6642c440e89a7f0b59eb996cab8a981ef9f
ahmedsalahacc <ahmedsalaheldin125@gmail.com>
2022-12-15 20:58:42 +0200

    remade the orderhistory activity and added connectors to the account fragment and order history and cart activities

--- app/src/main/java/com/ahmedsalah/wagabat/activities/OrderHistory.java
+++ app/src/main/java/com/ahmedsalah/wagabat/activities/OrderHistory.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.ahmedsalah.wagabat.R;
 
-public class OrdersHistoryActivity extends AppCompatActivity {
+public class OrderHistory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orders_history);
+        setContentView(R.layout.activity_order_history);
     }
 }
--- app/src/main/java/com/ahmedsalah/wagabat/fragments/AccountFragment.java
+++ app/src/main/java/com/ahmedsalah/wagabat/fragments/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.ahmedsalah.wagabat.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,48 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.ahmedsalah.wagabat.R;
+import com.ahmedsalah.wagabat.activities.CartActivity;
+import com.ahmedsalah.wagabat.activities.OrderHistory;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AccountFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AccountFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    View view;
+    Button ordersHistoryBtn, cartBtn, vouchersBtn,
+            paymentsBtn, helpBtn, aboutBtn, signoutBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        // init components
+        initComponents();
+
+        // event listeners
+        ordersHistoryBtn.setOnClickListener(v->{
+            replaceActivity(OrderHistory.class);
+        });
+        cartBtn.setOnClickListener(v->{
+            replaceActivity(CartActivity.class);
+        });
+
+        return view;
+    }
+
+    private void initComponents(){
+        ordersHistoryBtn = view.findViewById(R.id.account_orders);
+        cartBtn = view.findViewById(R.id.btn_cart);
+        vouchersBtn = view.findViewById(R.id.account_vouchers);
+        paymentsBtn = view.findViewById(R.id.account_payment);
+        helpBtn = view.findViewById(R.id.account_help);
+        aboutBtn = view.findViewById(R.id.account_about);
+        signoutBtn = view.findViewById(R.id.btn_signout);
+    }
+    private void replaceActivity(Class activity){
+        view.getContext().startActivity(new Intent(view.getContext(), activity));
     }
 }
