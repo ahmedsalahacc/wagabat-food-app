9f69f2dd759651cd67b84e9008802159b48ba76a
ahmedsalahacc <ahmedsalaheldin125@gmail.com>
2022-12-31 01:21:21 +0200

    added tracking functionality for orders in the webapp

--- webapp/src/static/js/home.js
+++ webapp/src/static/js/home.js
@@ -9,6 +9,7 @@ import {
   ref,
   set,
   get,
+  onValue,
   child,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
@@ -30,11 +31,28 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
+/**
+ *
+ * @param {*} status
+ * @returns
+ */
+const convertStatusToString = (status) => {
+  switch (Number.parseInt(status)) {
+    case 2:
+      return "Preparing";
+    case 3:
+      return "Delivered";
+    default:
+      return "Waiting for confirmation";
+  }
+};
+
 onAuthStateChanged(auth, async (user) => {
   if (user != null) {
     console.log("user", user);
     restId = user.uid;
     updateResturantWelcomeState();
+    updateOrdersForRestaurant();
   } else {
     alert("login required");
     console.log(user);
@@ -77,9 +95,6 @@ function uuidv4() {
     ).toString(16)
   );
 }
-
-/**  */
-function updateOrdersState() {}
 
 /**
  *
@@ -132,4 +147,153 @@ function updateResturantWelcomeState() {
     .catch((error) => {
       console.log(error);
     });
+}
+
+let activeOrders = [];
+let allOrders = [];
+
+function updateOrdersForRestaurant() {
+  async function getOrderData(orderId) {
+    let returnVal;
+    const orderPath = "orders/" + orderId;
+    await get(child(ref(database), orderPath)).then((snapshot) => {
+      if (snapshot.exists()) {
+        returnVal = snapshot.val();
+      }
+    });
+    return returnVal;
+  }
+
+  let firstTime = true;
+  const restaurantRef = "resturants/" + restId;
+  get(child(ref(database), restaurantRef))
+    .then((snapshot) => {
+      // do
+      if (snapshot.exists()) {
+        let orderIds = snapshot.val()["orders"];
+
+        for (let orderId of Object.keys(orderIds)) {
+          getOrderData(orderId).then((data) => {
+            if (Number.parseInt(data["status"]) === 1) {
+              activeOrders.push({ id: orderId, data: data });
+            } else {
+              allOrders.push({ id: orderId, data: data });
+            }
+            console.log("allorders", allOrders);
+            console.log("active orders", activeOrders);
+            updateActiveOrdersState();
+            updateAllOrdersState();
+          });
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(restaurantRef, "is not available");
+      console.log("ERR:" + error);
+    });
+}
+
+/**  */
+function updateActiveOrdersState() {
+  let activeOrdersHTMLContainerNode =
+    document.getElementById("active__container");
+  let innerHTMLContainer = "";
+  let collapseTracker = 1;
+  for (const each of activeOrders) {
+    // @TODO get dish items then do the rest
+    innerHTMLContainer += `
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapse${collapseTracker}" aria-expanded="true" aria-controls="collapse${collapseTracker}">
+                OrderID: ${each["id"]}
+            </button>
+        </h2>
+        <div id="collapse${collapseTracker}" class="accordion-collapse collapse" aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <p>
+                  <strong>Address:</strong> ${each["data"]["address"]}
+                </p>
+                <p>
+                  <strong>Datetime:</strong> ${each["data"]["datetime"]}
+                </p>
+                <p>
+                  <strong>Price:</strong> ${each["data"]["price"]}
+                </p>
+                <p>
+                  <strong>Status:</strong> ${convertStatusToString(
+                    each["data"]["status"]
+                  )}
+                </p>
+                <button class="btn btn-danger btn-order-confirm" id="${
+                  each["id"]
+                }">Confirm</button>
+            </div>
+        </div>
+    </div>
+    `;
+    collapseTracker++;
+  }
+  activeOrdersHTMLContainerNode.innerHTML = innerHTMLContainer;
+
+  // add event listeners to the buttons
+  const btns = document.getElementsByClassName("btn-order-confirm");
+  for (let each of btns) {
+    each.addEventListener("click", () => {
+      console.log(each.id);
+      changeOrderStatusToAccepted(each.id);
+    });
+  }
+}
+function updateAllOrdersState() {
+  let ordersHistoryHTMLContainerNode =
+    document.getElementById("orders__history");
+  let innerHTMLContainer = "";
+  for (const each of allOrders) {
+    // @TODO get dish items then do the rest
+    innerHTMLContainer += `
+    <div>
+        <div><strong>OrderID:</strong> ${each["id"]}</div>
+        <div><strong>Status:</strong> ${convertStatusToString(
+          each["data"]["status"]
+        )}</div>
+        <div><strong>Price:</strong> ${each["data"]["price"]}</div>
+        <div><strong>Datetime:</strong> ${each["data"]["datetime"]}</div>
+        <div><strong>Address:</strong> ${each["data"]["address"]}</div>
+    </div>
+    `;
+  }
+  ordersHistoryHTMLContainerNode.innerHTML = innerHTMLContainer;
+}
+
+async function changeOrderStatusToAccepted(id) {
+  const orderPath = "orders/" + id;
+
+  get(child(ref(database), orderPath))
+    .then(async (snapshot) => {
+      // do
+      if (snapshot.exists()) {
+        let order = snapshot.val();
+        await set(ref(database, orderPath), {
+          ...order,
+          status: 2,
+        }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log("error:" + errorMessage + ":" + errorCode);
+        });
+      }
+    })
+    .then(() => notifyChanged())
+    .catch((error) => {
+      console.log(orderPath, "is not available");
+      console.log("ERR:" + error);
+    });
+}
+
+function notifyChanged() {
+  activeOrders = [];
+  allOrders = [];
+  updateOrdersForRestaurant();
 }
